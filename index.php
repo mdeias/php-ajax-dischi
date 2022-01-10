0f74de1e4144843b5c53e5104a2d90b69f023865
mdeias <mdeias@icloud.com>
2022-01-10 19:41:12 +0100

    html e css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/database.php';
-var_dump($database);
+
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,26 @@ var_dump($database);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <div class="container">
+            <img src="img/logo-spotify.png" alt="">
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="card">
+                <div class="cover">
+                    
+                </div>
+                <h3>titolo</h3>
+                <p>artista</p>
+                <p>anno</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
