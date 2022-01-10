bbd71bd3ffd1520e8d90fca6c14557be80a2409c
mdeias <mdeias@icloud.com>
2022-01-10 23:55:56 +0100

    stampa dischi con vue

--- database.php
+++ database.php
@@ -69,7 +69,7 @@ $database = [
         'title' => 'Hard Wired',
         'author' => 'Dave Weckl',
         'year' => '1994',
-        'poster' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg"',
+        'poster' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg',
         'genre' => 'Jazz'
     ],
 
--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const app = new Vue({
 
     el:'#app',
     data:{
-
+        discs: [],
+        apiUrl: 'http://localhost:8888/php-ajax-dischi/server.php'
     },
     
     methods:{
         getData(){
-            console.log('get data');
+            axios.get(this.apiUrl).
+            then(res => {
+                this.discs = res.data;
+                console.log(res.data);
+            })
         }
     },
     mounted(){
